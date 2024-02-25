4c1a37dac0d68f7bdd162e3bb70104343c675215
Nummer42O <johannes.handy007@gmail.com>
2024-02-25 17:46:32 +0100

    added docstrings

--- include/new_game_dialog.hpp
+++ include/new_game_dialog.hpp
@@ -13,17 +13,36 @@ public:
   {
     WIN, LOSE,
     START, RESTART,
-    INVALID
+
+    NO_OPT
   };
 
   enum class ReturnType
   {
-    RESTART, QUIT, UNDO
+    RESTART, QUIT, UNDO,
+
+    NO_OPT
   };
 
 public:
+  /**
+   * @brief Create a new dialog for selecting the field size for the next game.
+   *
+   * @param parent dialog parent window
+   */
   NewGameDialog(Gtk::Window &parent);
 
+  /**
+   * @brief Run the dialog.
+   *
+   * @param type The type of information to show about the game state.
+   * @param io_rows user selected rows; initial value gets reflected in the scale
+   * @param max_rows the upper soft limit for the number of rows
+   * @param io_cols user selected columns; initial value gets reflected in the scale
+   * @param max_cols the upper soft limit for the number of columns
+   *
+   * @return the action the user selected
+   */
   ReturnType run(
     Type type,
     size_t &io_rows,
@@ -33,14 +52,12 @@ public:
   );
 
 private:
+  static const std::unordered_map<Type, Glib::ustring> type2text;
   static const int RESPONSE_UNDO = 1;
 
   MW_DECLARE_LOGGER;
 
   Gtk::HScale rows_widget, cols_widget;
-  Gtk::Label main_text;
-
+  Gtk::Label  main_text;
   Gtk::Button *undo_button;
-
-  static const std::unordered_map<Type, Glib::ustring> type2text;
 };
